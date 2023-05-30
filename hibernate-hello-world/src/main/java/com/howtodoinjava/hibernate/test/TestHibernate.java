7af7f3039f8bb79d207ad89704425ec5ba421d48
N-Eisen <107843956+N-Eisen@users.noreply.github.com>
2023-05-30 17:06:50 +0200

    Init

---
+++ hibernate-hello-world/src/main/java/com/howtodoinjava/hibernate/test/TestHibernate.java
@@ -0,0 +1,29 @@
+package com.howtodoinjava.hibernate.test;
+
+import org.hibernate.Session;
+
+import com.howtodoinjava.hibernate.test.dto.EmployeeEntity;
+
+public class TestHibernate {
+
+	public static void main(String[] args) {
+
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+
+		// Add new Employee object
+		EmployeeEntity emp = new EmployeeEntity();
+		emp.setEmail("demo-user@mail.com");
+		emp.setFirstName("demo");
+		emp.setLastName("user");
+
+		session.persist(emp);
+		
+
+		session.getTransaction().commit();
+		
+		System.out.println(session.contains(emp));
+		HibernateUtil.shutdown();
+	}
+
+}
